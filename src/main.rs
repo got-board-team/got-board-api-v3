57de727ff79dad0c6c588c1ba756e38ddae7a476
Rafael Borgonovi Tauil <rafael@tauil.com.br>
2020-04-01 00:42:47 +0200

    Fixes remaining errors

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use]
-extern crate rocket;
 extern crate diesel;
 extern crate dotenv;
+
+pub mod db;
+pub mod models;
+pub mod schema;
+
+#[macro_use]
+extern crate rocket;
+#[macro_use]
 extern crate rocket_contrib;
 
 use rocket_contrib::json::JsonValue;
-use serde_json::json;
 
 #[get("/matches")]
 fn read() -> JsonValue {
