516932cec6c07a51ea85b71475d6c91fd5af3f2d
Rafael Borgonovi Tauil <rafael@tauil.com.br>
2020-04-27 21:10:42 +0200

    Adds sql with seed data

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO pieces (match_id, piece_type, x, y, location, house_name) VALUES (1, 'footman', 0, 0, 'war-room', 'baratheon');
+INSERT INTO pieces (match_id, piece_type, x, y, location, house_name) VALUES (1, 'knight', 0, 0, 'war-room', 'baratheon');
+INSERT INTO pieces (match_id, piece_type, x, y, location, house_name) VALUES (1, 'siege', 100, 100, 'map', 'baratheon');
+INSERT INTO pieces (match_id, piece_type, x, y, location, house_name) VALUES (1, 'ship', 0, 0, 'war-room', 'baratheon');
+INSERT INTO pieces (match_id, piece_type, x, y, location, house_name, spec) VALUES (1, 'card', 0, 0, 'war-room', 'baratheon', '{"card": "brienne", "flipped": false}');
